886fd9718733c49d194c3f28a5ee123f7bbca64c
Japapino <stefan.murakami@gmail.com>
2018-07-19 11:03:22 -0500

    added in ssearch bar element and package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 //go find the react libray in the react folder and assign it the name 'React'
+//libraries
 import React from 'react';
 import ReactDOM from 'react-dom'; 
+
+//files
+import SearchBar from './components/search_bar.js'; 
+
+const API_KEY = 'AIzaSyCaGjnK_MBXANQrTSjA1tIE1HspBr2fEUk';
 
 //create new component. This component should make some html
 
@@ -10,7 +16,11 @@ import ReactDOM from 'react-dom';
 
 //using es6 arrow functions. The function of 'this' is slightly different here
 const App = () => { 
-	return <div>Hi!</div>
+	return (
+		<div>
+		<SearchBar /> 
+	</div>
+	);
 }
 //Take this component's generated html and put it in the DOM
 
