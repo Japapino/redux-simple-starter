8bd2dbf04032b756ceac7816e12ddda1f721a01f
Japapino <stefan.murakami@gmail.com>
2018-07-19 01:13:19 -0500

    added notes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 
 //create new component. This component should make some html
-const App = function() { 
+
+// const App = function() { 
+// 	return <div>Hi!</div>
+// }
+
+//using es6 arrow functions. The function of 'this' is slightly different here
+const App = () => { 
 	return <div>Hi!</div>
 }
-
 //Take this component's generated html and put it in the DOM
 
 // ReactDOM.render(App); //App is a class, not an instance
